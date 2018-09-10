94bfba370d87865c0c02a19fc16a9668c221ee89
Bianca <bianca.rocha1711@gmail.com>
2018-09-10 08:09:08 -0300

    showing link pages only if user is logged in and start on router protection

---
+++ src/helpers/auth.js
@@ -0,0 +1,28 @@
+import locationHelperBuilder from "redux-auth-wrapper/history4/locationHelper";
+import { connectedRouterRedirect } from "redux-auth-wrapper/history4/redirect";
+import Loading from "../components/layouts/Loading";
+const locationHelper = locationHelperBuilder({});
+
+export const UserIsAuthenticated = connectedRouterRedirect({
+  wrapperDisplayName: "UserIsAuthenticated",
+  AuthenticatingComponent: Loading,
+  allowRedirectBack: true,
+  redirectPath: (state, ownProps) =>
+    locationHelper.getRedirectQueryParam(ownProps) || "/login",
+  authenticatingSelector: ({ firebase: { auth, profile, isInitializing } }) =>
+    !auth.isLoaded || isInitializing === true,
+  authenticatedSelector: ({ firebase: { auth } }) =>
+    auth.isLoaded && !auth.isEmpty
+});
+
+export const UserIsNotAuthenticated = connectedRouterRedirect({
+  wrapperDisplayName: "UserIsNotAuthenticated",
+  AuthenticatingComponent: Loading,
+  allowRedirectBack: false,
+  redirectPath: (state, ownProps) =>
+    locationHelper.getRedirectQueryParam(ownProps) || "/",
+  authenticatingSelector: ({ firebase: { auth, isInitializing } }) =>
+    !auth.isLoaded || isInitializing === true,
+  authenticatedSelector: ({ firebase: { auth } }) =>
+    auth.isLoaded && auth.isEmpty
+});
